9437a453bb55dfbc99aae5d0b4f6729cef1eebf1
Spider <silentwalk15@gmail.com>
2021-10-31 19:15:22 +0100

    Added two unit tests that asserts on the size of a list of TV programs.

---
+++ rest-service/src/test/java/com/epg/restservice/tools/EpgJsonToJavaTest.java
@@ -0,0 +1,67 @@
+package com.epg.restservice.tools;
+
+import com.epg.restservice.models.DailyTvProgram;
+import com.epg.restservice.models.Epg;
+import com.epg.restservice.models.TvProgram;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpgJsonToJavaTest {
+    private List<DailyTvProgram> monday;
+    private List<DailyTvProgram> tuesday;
+    private List<DailyTvProgram> wednesday;
+    private List<DailyTvProgram> thursday;
+    private List<DailyTvProgram> friday;
+    private List<DailyTvProgram> saturday;
+    private List<DailyTvProgram> sunday;
+
+    private DailyTvProgram dailyTvProgram1;
+    private DailyTvProgram dailyTvProgram2;
+    Epg epg;
+
+    @BeforeEach
+    void prepareData() {
+        monday = new ArrayList<>();
+        tuesday = new ArrayList<>();
+        wednesday = new ArrayList<>();
+        thursday = new ArrayList<>();
+        friday = new ArrayList<>();
+        saturday = new ArrayList<>();
+        sunday = new ArrayList<>();
+
+
+    }
+
+    @Test
+    void checkThatItOnlyCreatesOneTvProgram() {
+        dailyTvProgram1 = new DailyTvProgram("Nyhederne", "begin", 3600);
+        dailyTvProgram2 = new DailyTvProgram("Nyhederne", "end", 6700);
+        monday.add(dailyTvProgram1);
+        monday.add(dailyTvProgram2);
+
+        epg = new Epg(monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+
+        EpgJsonToJava epgJsonToJava = new EpgJsonToJava();
+        List<TvProgram> actual = epgJsonToJava.processEpgJson(epg);
+        int expected = 1;
+
+        assertEquals(expected, actual.size());
+    }
+
+    @Test
+    void checkWhenEmptyThatNothingIsParsed() {
+        epg = new Epg(monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+
+        EpgJsonToJava epgJsonToJava = new EpgJsonToJava();
+        List<TvProgram> actual = epgJsonToJava.processEpgJson(epg);
+        int expected = 0;
+
+        assertEquals(expected, actual.size());
+    }
+}
